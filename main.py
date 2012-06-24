1e9b72960955ae62709b7c0035b236ee7b736a9e
Ahmad Luqman <ahmad.luqman@gmail.com>
2012-06-24 19:23:27 +0500

    Revert "Revert "Add form to the response""
    This reverts commit 736ca10ceedab82a6fcf87e80189ce5d6929567c.

--- main.py
+++ main.py
@@ -16,9 +16,16 @@
 #
 import webapp2
 
+form = """
+<form action="http://www.google.com/search">
+    <input name="q">
+	<input type="submit">
+</form>
+"""
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        self.response.out.write(form)
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
                               debug=True)
