f90cf4608296219f22c6af7d0c3de6e1a53408b0
Ahmad Luqman <ahmad.luqman@gmail.com>
2012-06-24 19:51:25 +0500

    Commit form text goes to write Add another handler which writes form input text to page

--- main.py
+++ main.py
@@ -17,7 +17,7 @@
 import webapp2
 
 form = """
-<form action="http://www.google.com/search">
+<form action="/testform">
     <input name="q">
 	<input type="submit">
 </form>
@@ -27,5 +27,11 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.out.write(form)
 
-app = webapp2.WSGIApplication([('/', MainHandler)],
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        q = self.request.get("q")
+        self.response.out.write(q)
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/testform', TestHandler)],
                               debug=True)
